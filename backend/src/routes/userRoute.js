f8c30fcb7ff9d4e713fb10560446f469549acc7f
lucassukar <lfsw@cesar.school>
2026-07-21 16:32:32 -0300

    feat(sessioncontroller): cria a sessao do user usando jwt e nova rota post no userRoute

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import SessionController from "../controllers/SessionController";
 
 const userRoute = new Router();
 
 userRoute.post("/users", UserController.store);
+
+userRoute.post("/sessions", SessionController.store);
 
 export default userRoute;
