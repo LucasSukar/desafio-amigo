768e65501960db28cf199a5ff3cc7e1cd9deca01
lucas sukar <lfsw@cesar.school>
2026-07-21 11:09:47 -0300

    feat(.sequelizerc, database.js): inicializa a conexão com o banco

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "098321",
+  database: "desafio-amigo",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
