0e468738aa34c722541be54257571afea3807e5b
lucas sukar <lfsw@cesar.school>
2026-07-25 14:03:50 -0300

    feat: cria a parte de deletar e editar post do usuario, cria botao com 3 pontos para escolher achao, cria containers para editar ou ecluir, refatora html para separar com pastas de css

--- frontend/js/services/FeedService.js
+++ frontend/js/services/FeedService.js
@@ -1,12 +1,40 @@
-angular.module("amigoApp").factory("FeedService", function($http, config){
-  var _getPosts = function() {
+angular.module("amigoApp").factory("FeedService", function ($http, config, LoginService) {
+  var _getHeaders = function () {
+    return {
+      headers: { Authorization: "Bearer " + LoginService.obterToken() },
+    };
+  };
+
+  var _getPosts = function () {
     return $http.get(config.baseUrl + "/post");
-  }
-  var _getPost = function() {
+  };
+
+  var _getPost = function (id) {
     return $http.get(config.baseUrl + "/post/" + id);
-  }
-  return{
+  };
+
+  var _getUserPost = function () {
+    return $http.get(config.baseUrl + "/post/me", _getHeaders());
+  };
+
+  var _postPost = function (post) {
+    return $http.post(config.baseUrl + "/post", post, _getHeaders());
+  };
+
+  var _putPost = function (id, post) {
+    return $http.put(config.baseUrl + "/post/" + id, post, _getHeaders());
+  };
+
+  var _deletePost = function (id) {
+    return $http.delete(config.baseUrl + "/post/" + id, _getHeaders());
+  };
+
+  return {
     getPosts: _getPosts,
     getPost: _getPost,
-  }
-})  
+    getUserPost: _getUserPost,
+    postPost: _postPost,
+    putPost: _putPost,
+    deletePost: _deletePost,
+  };
+});
--- frontend/js/services/PerfilService.js
+++ frontend/js/services/PerfilService.js
@@ -1,9 +1,32 @@
-angular.module("amigoApp").factory("PerfilService", function($http, config){
-  var _postUser = function(usuario) {
-    return $http.post(config.baseUrl + "/users", usuario);
-  }
+angular
+  .module("amigoApp")
+  .factory("PerfilService", function ($http, config, LoginService) {
+    var _getHeaders = function () {
+      return {
+        headers: { Authorization: "Bearer " + LoginService.obterToken() },
+      };
+    };
 
-  return{
-    postUser: _postUser
-  }
-})
+    var _postUser = function (usuario) {
+      return $http.post(config.baseUrl + "/users", usuario);
+    };
+
+    var _getUserPosts = function () {
+      return $http.get(config.baseUrl + "/post/me", _getHeaders());
+    };
+
+    var _putPost = function (id, post) {
+      return $http.put(config.baseUrl + "/post/" + id, post, _getHeaders());
+    };
+
+    var _deletePost = function (id) {
+      return $http.delete(config.baseUrl + "/post/" + id, _getHeaders());
+    };
+
+    return {
+      postUser: _postUser,
+      getUserPosts: _getUserPosts,
+      putPost: _putPost,
+      deletePost: _deletePost,
+    };
+  });
