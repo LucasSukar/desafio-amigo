f2e8a276988902a6ff453c56bc149adbc44a9c2f
lucassukar <lfsw@cesar.school>
2026-07-29 14:08:04 -0300

    feat: adiciona fucionalidade de ditar usuarioi

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
 import SessionController from "../controllers/SessionController";
+import auth from "../middlewares/auth";
 
 const userRoute = new Router();
 
 userRoute.post("/users", UserController.store);
-
 userRoute.post("/sessions", SessionController.store);
 
+userRoute.put("/users", auth, UserController.update);
+
 export default userRoute;
+
