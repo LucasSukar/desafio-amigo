5a8583e3ecceb9b7c29e7d11563d2995e294a2f9
lucas sukar <lfsw@cesar.school>
2026-08-04 19:29:07 -0300

    fix: refatora os contrllers, adiciona pasta de schemas e constants

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,17 +1,18 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import auth from "../config/auth";
+import { AUTH_MESSAGES } from "../constants/messages";
 
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(401).json({ error: "usuario nao existe" });
+      return res.status(401).json({ error: AUTH_MESSAGES.USER_NOT_FOUND });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: "senha incorreta" });
+      return res.status(401).json({ error: AUTH_MESSAGES.WRONG_PASSWORD });
     }
     const { id, name } = user;
     return res.json({
